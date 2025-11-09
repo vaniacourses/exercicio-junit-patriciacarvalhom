9e472ebb5f4457627a66e5b5f856bad14bcb6181
Patricia Carvalho <patriciacarvalho@id.uff.br>
2025-11-09 00:32:27 -0300

    Correções

--- test/calculadora/CalculadoraTesteAluna.java
+++ test/calculadora/CalculadoraTesteAluna.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-@DisplayName("Classe para testes da calculadora da aluna")
+@DisplayName("Classe da aluna para testes da calculadora")
 public class CalculadoraTesteAluna {
     private Calculadora calc;
 
@@ -23,43 +23,43 @@ public class CalculadoraTesteAluna {
 
     @DisplayName("Testa a subtração")
     @Test
-    void testeSubtracao() {
+    public void testeSubtracao() {
         assertEquals(-1, calc.subtracao(2, 3));
     }
 
     @DisplayName("Testa a multiplicação")
     @Test
-    void testeMultiplicacao() {
+    public void testeMultiplicacao() {
         assertEquals(6, calc.multiplicacao(2, 3));
     }
 
     @DisplayName("Testa a divisão")
     @Test
-    void testeDivisao() {
+    public void testeDivisao() {
         assertEquals(2, calc.divisao(6, 3));
     }
 
     @DisplayName("Testa se número é positivo")
     @Test
-    void testeEhPositivo_verdadeiro() {
+    public void testeEhPositivo_verdadeiro() {
         assertTrue(calc.ehPositivo(5));
     }
 
     @DisplayName("Testa compara: A é maior que B")
     @Test
-    void testeComparaMaior() {
+    public void testeComparaMaior() {
         assertEquals(1, calc.compara(5, 3));
     }
 
     @DisplayName("Testa compara: A é menor que B")
     @Test
-    void testeComparaMenor() {
+    public void testeComparaMenor() {
         assertEquals(-1, calc.compara(3, 5));
     }
 
     @DisplayName("Testa compara: A e B são iguais")
     @Test
-    void testeComparaIgual() {
+    public void testeComparaIgual() {
         assertEquals(0, calc.compara(5, 5));
     }
 }
