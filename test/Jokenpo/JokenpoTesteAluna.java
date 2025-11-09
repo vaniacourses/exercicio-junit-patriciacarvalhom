7ce6fd9e03011c51fa17a4fbf4f8c287435f6e49
Patricia Carvalho <patriciacarvalho@id.uff.br>
2025-11-09 01:11:46 -0300

    Inclusão do jokenpo e criação da classe de testes dele

---
+++ test/Jokenpo/JokenpoTesteAluna.java
@@ -0,0 +1,17 @@
+package Jokenpo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Classe para teste do Jokenpo")
+public class JokenpoTesteAluna {
+    
+    
+}
