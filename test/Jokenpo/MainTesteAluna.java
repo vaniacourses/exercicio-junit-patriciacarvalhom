8bd91c1ca06d4851e2c41b418a444ff9b85f1708
Patricia Carvalho <patriciacarvalho@id.uff.br>
2025-11-09 01:34:51 -0300

    Inclusão de testes para jokenpo

--- test/Jokenpo/MainTesteAluna.java
+++ test/Jokenpo/MainTesteAluna.java
@@ -1,5 +1,6 @@
 package Jokenpo;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,7 +12,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Classe para teste do Jokenpo")
-public class JokenpoTesteAluna {
+public class MainTesteAluna {
+    private Main jokenpo;
+
+    @BeforeEach
+    public void instanciando(){
+        jokenpo = new Main();
+    }
     
-    
+    @DisplayName("Testando empates")
+    @Test
+    void testeEmpates() {
+        assertEquals(0, jokenpo.jogar(1, 1), "Papel x Papel é empate");
+        assertEquals(0, jokenpo.jogar(2, 2), "Pedra x Pedra é ser empate");
+        assertEquals(0, jokenpo.jogar(3, 3), "Tesoura x Tesoura é empate");
+    }
+
+    @DisplayName("Testando a vitória do jogador 1")
+    @Test
+    void testeJogador1Vence() {
+        
+        assertEquals(1, jokenpo.jogar(1, 2), "Jogador 1 (Papel) vence Jogador 2 (Pedra)");
+        assertEquals(1, jokenpo.jogar(2, 3), "Jogador 1 (Pedra) vence Jogador 2 (Tesoura)");
+        assertEquals(1, jokenpo.jogar(3, 1), "Jogador 1 (Tesoura) vence Jogador 2 (Papel)");
+    }
+
+    @DisplayName("Testando a vitória do jogador 2")
+    @Test
+    void testeJogador2Vence() {
+
+        assertEquals(2, jokenpo.jogar(2, 1), "Jogador 2 (Papel) vence Jogador 1 (Pedra)");
+        assertEquals(2, jokenpo.jogar(3, 2), "Jogador 2 (Pedra) vence Jogador 1 (Tesoura)");
+        assertEquals(2, jokenpo.jogar(1, 3), "Jogador 2 (Tesoura) vence Jogador 1 (Papel)");
+    }
+
+
+
 }
