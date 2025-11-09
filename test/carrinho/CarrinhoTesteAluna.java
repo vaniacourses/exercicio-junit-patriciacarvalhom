81b04fb51bc62addc86102b3175833b8c612c20a
Patricia Carvalho <patriciacarvalho@id.uff.br>
2025-11-08 22:06:50 -0300

    Criação da classe de teste para carrinho

---
+++ test/carrinho/CarrinhoTesteAluna.java
@@ -0,0 +1,23 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package carrinho;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Classe para testes da classe carrinho da aluna")
+public class CarrinhoTesteAluna {
+
+    private Carrinho car;
+
+    public CarrinhoTesteAluna(){
+    }
+
+    @BeforeEach
+    public void instanciando() {
+      this.car = new Carrinho();
+   }
+
+   
+}
